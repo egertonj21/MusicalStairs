f7001ab37bd9c1c0ea41ecdcff2509468f2e0b30
egertonj21 <egertonjoel@gmail.com>
2024-07-20 13:09:35 +0100

    added security mode functionality

--- mqtt_handler.py
+++ mqtt_handler.py
@@ -7,7 +7,7 @@ import json
 from sensor_data import fetch_and_play_note_details
 from config import (MQTT_BROKER, MQTT_PORT, MQTT_TOPICS, MQTT_MUTE_TOPIC, CONTROL_TOPIC, 
                     MOTION_CONTROL_TOPIC, WS_SERVER_URL)
-from utils import retry_request
+from utils import retry_request, get_current_mode
 
 # Configure logging
 logger = logging.getLogger(__name__)
@@ -19,7 +19,7 @@ NUM_LEDS = 31
 
 # Timeout period for ultrasonic sensors to sleep (in seconds)
 TIMEOUT_PERIOD = 300  # 5 minutes
-ALIVE_CHECK_PERIOD = 5  # Period to check for alive messages (in seconds)
+ALIVE_CHECK_PERIOD = 60  # Period to check for alive messages (in seconds)
 
 # Dictionary to track the last activity time for each ultrasonic sensor
 last_activity = {sensor_id: time.time() for sensor_id in range(1, 5)}
@@ -203,5 +203,3 @@ def setup_mqtt_client():
     client.on_message = on_message
     client.connect(MQTT_BROKER, MQTT_PORT)
     return client
-
-
--- utils.py
+++ utils.py
@@ -1,23 +1,32 @@
 import time
 import logging
+import json
+import websocket
+from config import WS_SERVER_URL
 
 # Configure logging
 logger = logging.getLogger(__name__)
 
+def fetch_all_positions():
+    try:
+        ws = websocket.WebSocket()
+        ws.connect(WS_SERVER_URL)
+        payload = {"action": "fetchAllPositions"}
+        ws.send(json.dumps(payload))
+        response = ws.recv()
+        response_data = json.loads(response)
+        ws.close()
+
+        if response_data.get("action") == "fetchAllPositions" and "data" in response_data:
+            return response_data["data"]
+        else:
+            logger.error("Failed to fetch positions")
+            return []
+    except Exception as e:
+        logger.error(f"Failed to fetch positions: {e}")
+        return []
+
 def retry_request(ws, payload, retries=5, delay=2):
-    """
-    A utility function to perform WebSocket requests with retries.
-
-    Args:
-        ws (WebSocket): The WebSocket instance.
-        payload (dict): The payload to send.
-        retries (int): The number of retry attempts.
-        delay (int): Delay between retries in seconds.
-
-    Returns:
-        Response: The response object if the request is successful.
-        None: If all retries fail.
-    """
     for attempt in range(retries):
         try:
             ws.send(json.dumps(payload))
@@ -30,15 +39,46 @@ def retry_request(ws, payload, retries=5, delay=2):
     return None
 
 def log_response(response):
-    """
-    Logs the details of an HTTP response.
-
-    Args:
-        response (requests.Response): The HTTP response to log.
-    """
     if response:
         logger.info(f"Response status: {response.status_code}")
         logger.debug(f"Response headers: {response.headers}")
         logger.debug(f"Response text: {response.text}")
     else:
         logger.error("No response to log.")
+
+def get_current_mode():
+    try:
+        ws = websocket.WebSocket()
+        ws.connect(WS_SERVER_URL)
+        payload = {"action": "fetchActiveMode"}
+        ws.send(json.dumps(payload))
+        response = ws.recv()
+        response_data = json.loads(response)
+        ws.close()
+        if response_data.get("action") == "fetchActiveMode" and "data" in response_data:
+            return response_data["data"].get("mode_ID")
+        else:
+            logger.error(f"Failed to fetch current mode: {response_data.get('error')}")
+            return None
+    except Exception as e:
+        logger.error(f"Failed to fetch current mode: {e}")
+        return None
+
+def fetch_security_sequences():
+    try:
+        ws = websocket.WebSocket()
+        ws.connect(WS_SERVER_URL)
+        payload = {"action": "fetchAllSecuritySequences"}
+        ws.send(json.dumps(payload))
+        response = ws.recv()
+        response_data = json.loads(response)
+        ws.close()
+        
+        if response_data.get("action") == "fetchAllSecuritySequences" and "data" in response_data:
+            return response_data["data"]
+        else:
+            logger.error("Failed to fetch security sequences")
+            return []
+    except Exception as e:
+        logger.error(f"Failed to fetch security sequences: {e}")
+        return []
